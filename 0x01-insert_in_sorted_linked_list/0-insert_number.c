c8f77ac062764d7f791f60af989d895dde698846
Juan8bits <juan160820@gmail.com>
2021-07-29 22:08:07 +0000

    Add edge case for NULL SLL

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -12,7 +12,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *copy_head;
 	listint_t *new;
 
-	if (!head)
+	if (!head || !*head)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
