5947cbdcecd25633113fcd819c0cc9a5647ea6ae
juan8bits <juan160820gmail.com>
2021-08-05 22:38:46 -0500

    Betty style correction

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -7,16 +7,16 @@
  * @value: Is the value to put in the new node.
  * Return: Pointer to the new node or NULL on failure.
  */
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new;
+	binary_tree_t *new;
 
-        new = malloc(sizeof(binary_tree_t));
-        if (!new)
-                return NULL;
-        new->n = value;
-        new->parent = parent;
-        new->left = NULL;
-        new->right = NULL;
-        return (new);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
 }
